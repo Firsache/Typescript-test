b3563adc01781841d3b1fa1925af4bb45547f03b
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-17 11:58:55 +0200

    added tsconfig.json

---
+++ src/app.ts
@@ -0,0 +1,115 @@
+const button = document.querySelector("button");
+const input1 = document.getElementById("num1")! as HTMLInputElement;
+const input2 = document.getElementById("num2")! as HTMLInputElement;
+
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+button?.addEventListener("click", function () {
+  console.log(add(+input1.value, +input2.value));
+});
+// ==========================================
+
+// let age: number;
+// age = 50;
+// let name: string;
+// name = "Max";
+// let toggle: boolean;
+// toggle = true;
+// let empty: null;
+// empty = null;
+// let notInitialize: undefined;
+// notInitialize = undefined;
+
+// let callback = (a: number) => number;
+// callback = (a) => {
+//   return 100 + a;
+// };
+// ==========================================
+
+// let anything: any;
+// anything = -20;
+// anything = "Text";
+// anything = {};
+
+// ==========================================
+
+// let some: unknown;
+// some = "Text";
+
+// let str: string;
+
+// if (typeof some === "string") {
+//   str = some;
+// }
+
+// ==========================================
+
+// let person: [string, number];
+// person = ["Max", 21];
+
+// ==========================================
+
+// enum StatusLoading {
+//   LOADING,
+//   READY,
+// }
+// const page = {
+//   status: StatusLoading.LOADING,
+// };
+
+// if (page.status === StatusLoading.LOADING) {
+//   console.log("Loading");
+// }
+
+// if (page.status === StatusLoading.READY) {
+//   console.log("Ready");
+// }
+
+// ==========================================
+
+// let arg: string | number;
+// let arg2: "enable" | "disable";
+// ==========================================
+
+// function showMessage(message: string): void {
+//   console.log(message);
+// }
+
+// function calc(num1: number, num2: number): number {
+//   return num1 + num2;
+// }
+
+// function customError(): never {
+//   throw new Error("Error");
+// }
+// ==========================================
+
+// const Page: {
+//   title: string;
+//   likes: number;
+//   accounts: string[];
+//   status: "open" | "close";
+//   details?: {
+//     createAt: string;
+//     updateAt: string;
+//   };
+// };
+// const page1 = {
+//   title: "The awesome page",
+//   likes: 100,
+//   accounts: ["Max", "Anton", "Nikita"],
+//   status: "open",
+//   details: {
+//     createAt: "2021-01-01",
+//     updateAt: "2021-05-01",
+//   },
+// };
+
+// const page2 = {
+//   title: "Python or Js",
+//   likes: 5,
+//   accounts: ["Alex"],
+//   status: "close",
+// };
