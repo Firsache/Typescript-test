904fafae4575478008a86b4e427471c9e04b3c98
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-19 23:57:41 +0200

    added classes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,27 @@
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-function add(num1, num2) {
-    return num1 + num2;
-}
-button === null || button === void 0 ? void 0 : button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value));
-});
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+// button?.addEventListener("click", function () {
+//   console.log(add(+input1.value, +input2.value));
+// });
 // ==========================================
 // let age: number;
 // age = 50;
@@ -91,3 +106,54 @@ button === null || button === void 0 ? void 0 : button.addEventListener("click",
 //   accounts: ["Alex"],
 //   status: "close",
 // };
+// ==========================================
+var House = /** @class */ (function () {
+    function House(key) {
+        this.key = key;
+        this.door = false;
+        this.tenants = [];
+    }
+    House.prototype.comeIn = function (person) {
+        if (!this.door) {
+            throw new Error("You can't come in.Door is close");
+        }
+        this.tenants.push(person);
+    };
+    return House;
+}());
+var Key = /** @class */ (function () {
+    function Key() {
+        this.signature = Math.random();
+    }
+    Key.prototype.getSignature = function () {
+        return this.signature;
+    };
+    return Key;
+}());
+var Person = /** @class */ (function () {
+    function Person(key) {
+        this.key = key;
+    }
+    Person.prototype.getKey = function () {
+        return this.key;
+    };
+    return Person;
+}());
+var MyHouse = /** @class */ (function (_super) {
+    __extends(MyHouse, _super);
+    function MyHouse() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    MyHouse.prototype.openDoor = function (key) {
+        if (key.getSignature() !== this.key.getSignature()) {
+            throw new Error("Key to another door");
+        }
+        return (this.door = true);
+    };
+    return MyHouse;
+}(House));
+var key = new Key();
+var house = new MyHouse(key);
+var person = new Person(key);
+house.openDoor(person.getKey());
+house.comeIn(person);
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
-}
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
 
-button?.addEventListener("click", function () {
-  console.log(add(+input1.value, +input2.value));
-});
+// button?.addEventListener("click", function () {
+//   console.log(add(+input1.value, +input2.value));
+// });
 // ==========================================
 
 // let age: number;
@@ -113,3 +113,49 @@ button?.addEventListener("click", function () {
 //   accounts: ["Alex"],
 //   status: "close",
 // };
+// ==========================================
+abstract class House {
+  protected door = false;
+  private tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+  comeIn(person: Person): void {
+    if (!this.door) {
+      throw new Error("You can't come in.Door is close");
+    }
+    this.tenants.push(person);
+  }
+  abstract openDoor(key: Key): boolean;
+}
+
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature() {
+    return this.signature;
+  }
+}
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getSignature() !== this.key.getSignature()) {
+      throw new Error("Key to another door");
+    }
+    return (this.door = true);
+  }
+}
+const key = new Key();
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+house.comeIn(person);
